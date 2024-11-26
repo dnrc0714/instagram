cf4bfdefbe40bcf0485056b9d16e51b255315dce
dnrc0____0 <ziziang951@gmail.com>
2024-11-26 17:12:04 +0900

    url 하드코딩 수정

--- components/auth/callback.tsx
+++ components/auth/callback.tsx
@@ -4,7 +4,7 @@ export default async function CallBack(session) {
     const userId = session?.user?.id;
     const providerToken = session?.provider_token
 
-    const response = await fetch('https://kapi.kakao.com/v2/user/me', {
+    const response = await fetch(process.env.NEXT_KAKAO_PROFILE_REST_API, {
         headers: {
             Authorization: `Bearer ${providerToken}`,
         },
