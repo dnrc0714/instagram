482d497c108e2862eb3a2613272a2e2565e752b2
dnrc0714 <ziziang951@gmail.com>
2024-12-08 00:06:17 +0900

    build error modify

--- components/feed/FeedList.tsx
+++ components/feed/FeedList.tsx
@@ -3,7 +3,7 @@
 import Feed from "./Feed";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { useInView } from "react-intersection-observer";
-import { getUserFeeds } from "actions/feedActions";
+import { getUserFeeds } from "actions/FeedActions";
 import { useEffect } from "react";
 import { Spinner } from "@material-tailwind/react";
 import { useRecoilValue } from "recoil";
