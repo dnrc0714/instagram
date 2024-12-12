c0d886b4ea79ee2e6ff7560e3ea4d541405d5157
dnrc0____0 <ziziang951@gmail.com>
2024-12-12 16:14:25 +0900

    error 수정

--- app/[userId]/feed/add/page.tsx
+++ app/[userId]/feed/add/page.tsx
@@ -5,12 +5,10 @@ import MyInfo from "components/feed/MyInfo";
 import { createServerSupabaseClient } from "utils/supabase/server";
 
 export default async function myFeedPage() {
-    const supabase = await createServerSupabaseClient();
-    const {data: {user}} = await supabase.auth.getUser();
     
     return (
         <div className="w-full h-screen flex justify-center items-center">
-            <AddFeed loggedInUser={user}/>
+            <AddFeed/>
         </div>
     );
 }
